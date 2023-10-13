c94dfd4c58607755d3f11bfa21bc02ad34b02de7
mogmarsh <mogmarsh@users.noreply.github.com>
2023-10-13 22:48:51 +0000

    Built changes for v0.1.0

---
+++ build/section/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '37537954a18418023de7');

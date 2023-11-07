91a4ceb434c2d05b7c24ec5c9bbebcda765f16a7
mogmarsh <mogmarsh@users.noreply.github.com>
2023-11-07 15:02:49 +0000

    Built changes for v0.2.0

---
+++ build/editor/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a73e01d1b0eb49b5942b');
---
+++ build/two-up-post/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '8c324b7c5d84eec90d61');

15cbb43c1cdfb66ddd230b50c7cbfebb855f66c0
mogmarsh <mogmarsh@users.noreply.github.com>
2023-11-08 16:13:40 +0000

    Built changes for v0.3.2

---
+++ build/wp-newsletter-builder-from-post/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'c02808dc2ce30da6699f');

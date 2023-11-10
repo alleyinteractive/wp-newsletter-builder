33033c317331b1a4eae9e16f83579829b73084a8
nikkifurls <nikkifurls@users.noreply.github.com>
2023-11-10 21:38:57 +0000

    Built changes for v0.3.3

---
+++ build/email-settings/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9f33b5ecf1fb21e934b7');

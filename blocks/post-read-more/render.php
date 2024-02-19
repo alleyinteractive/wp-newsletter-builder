6f353e2ed369cb9a4e9fb1a83587712187413bda
Nicole Furlan <nfurlan@alley.com>
2024-02-19 17:07:13 -0500

    Resolves issues flagged by `phpstan` in `blocks/post*/render.php`

--- blocks/post-read-more/render.php
+++ blocks/post-read-more/render.php
@@ -12,18 +12,20 @@
 $wp_newsletter_builder_block_post = $block->context['postId'] ?? null;
 
 $wp_newsletter_builder_block_post = get_post( $wp_newsletter_builder_block_post );
-if ( empty( $wp_newsletter_builder_block_post ) || ! $wp_newsletter_builder_block_post ) {
+if ( empty( $wp_newsletter_builder_block_post ) ) {
 	return;
 }
-$wp_newsletter_builder_read_more_text = $attributes['readMoreText'] ?: __( 'Read More', 'wp-newsletter-builder' );
+$wp_newsletter_builder_read_more_text = $attributes['readMoreText'] ?? __( 'Read More', 'wp-newsletter-builder' );
+
+$wp_newsletter_builder_post_url = get_the_permalink();
 ?>
 <div <?php echo wp_kses_data( get_block_wrapper_attributes( [ 'class' => 'wp-block-button has-text-align-center' ] ) ); ?>>
 	<!--[if mso]>
-	<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="<?php echo esc_url( $wp_newsletter_builder_post_url ); ?>" style="height:48px;v-text-anchor:middle;width:200px;" arcsize="10%" stroke="f" fillcolor="#D62827">
+	<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="<?php echo esc_url( ! empty( $wp_newsletter_builder_post_permalink ) ? $wp_newsletter_builder_post_permalink : '' ); ?>" style="height:48px;v-text-anchor:middle;width:200px;" arcsize="10%" stroke="f" fillcolor="#D62827">
 		<w:anchorlock/>
 		<center>
 	<![endif]-->
-	<a class="wp-element-button" href="<?php echo esc_url( get_the_permalink() ); ?>">
+	<a class="wp-element-button" href="<?php echo esc_url( ! empty( $wp_newsletter_builder_post_permalink ) ? $wp_newsletter_builder_post_permalink : '' ); ?>">
 		<?php echo esc_html( $wp_newsletter_builder_read_more_text ); ?>
 	</a>
 	<!--[if mso]>

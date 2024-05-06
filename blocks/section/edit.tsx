657c0106c9dfcd5aec8dfe07b126c7b9f6bde10c
Nicole Furlan <nfurlan@alley.com>
2024-05-06 09:55:16 -0400

    Removes no longer needed `applyFilters` import

--- blocks/section/edit.tsx
+++ blocks/section/edit.tsx
@@ -8,7 +8,6 @@ import { dispatch, select } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { PanelBody, PanelRow } from '@wordpress/components';
 import { useEffect } from '@wordpress/element';
-import { applyFilters } from '@wordpress/hooks';
 
 import MultiplePostPicker from '@/components/multiplePostPicker';
 import PostPickerResult from '@/components/postPickerResult';

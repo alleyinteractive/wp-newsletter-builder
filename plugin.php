4bbaf3eaa36679ea5cb0ca0fd634beb897062858
cahdeemer <christina@joinlede.com>
2024-05-21 13:17:16 -0400

    bump version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Newsletter Builder
  * Plugin URI: https://github.com/alleyinteractive/wp-newsletter-builder
  * Description: Interface to manage email newsletters
- * Version: 0.3.11
+ * Version: 0.3.12
  * Author: Alley Interactive
  * Author URI: https://github.com/alleyinteractive/wp-newsletter-builder
  * Requires at least: 6.2

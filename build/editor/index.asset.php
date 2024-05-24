dd922721c53196a7f42f9d67c465c6aa9efd23c6
cahdeemer <cahdeemer@users.noreply.github.com>
2024-05-24 13:36:55 +0000

    Built changes for v0.3.13 from develop 31f55d4d120a56d9ad29e3975855ffb56a451a71

---
+++ build/editor/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'affb44c713fb93b9551d');
---
+++ build/email-settings/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd1011d1f268d36094759');

d6b05e83c754de1e101a8bce4cda97234de59b52
cahdeemer <christina@joinlede.com>
2024-06-04 09:40:32 -0400

    lint

--- blocks/divider/index.php
+++ blocks/divider/index.php
@@ -12,11 +12,10 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function divider_divider_block_init() {
+function wp_newsletter_builder_divider_block_init() {
 	// Register the block by passing the location of block.json.
 	register_block_type(
 		__DIR__
 	);
-
 }
-add_action( 'init', 'divider_divider_block_init' );
+add_action( 'init', 'wp_newsletter_builder_divider_block_init' );

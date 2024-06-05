76e29492a9847cc6ee2f9ce272d5375a7d407bdc
cahdeemer <cahdeemer@users.noreply.github.com>
2024-06-05 13:55:00 +0000

    Built changes for v0.3.17 from develop 998bc1e6ff7b19bde65b846d7004f17437e6865d

---
+++ build/section/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fa06e9f952c3f72aa412');

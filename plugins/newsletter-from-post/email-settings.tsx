b42c42bfd643e324306560c11b1756ed0bbca993
efuller <eric@alley.co>
2024-06-11 10:33:17 -0400

    Extract a hook for managing newsletter meta

--- plugins/newsletter-from-post/email-settings.tsx
+++ plugins/newsletter-from-post/email-settings.tsx
@@ -12,11 +12,11 @@ import { __ } from '@wordpress/i18n';
 import { useMemo, useEffect, useState } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import apiFetch from '@wordpress/api-fetch';
-import { usePostMeta } from '@alleyinteractive/block-editor-tools';
 import { MultiSelect } from 'react-multi-select-component';
 import { parse, serialize } from '@wordpress/blocks';
-
+import useNewsletterMeta from '@/plugins/newsletter-from-post/useNewsletterMeta';
 import NewsletterSpinner from '@/components/newsletterSpinner';
+
 import EmailTypeSelector from '../../components/emailTypeSelector';
 import SentNewsletter from './sent-newsletter';
 
@@ -43,21 +43,8 @@ interface Window {
 }
 
 function EmailSettings() {
-  const [meta, setMeta] = usePostMeta();
+  const { meta, setMeta } = useNewsletterMeta();
   const [lists, setLists] = useState<ListResult[]>([]); // eslint-disable-line
-
-  const {
-    nb_breaking_email_type: type = '',
-    nb_breaking_template: template = '',
-    nb_breaking_from_name: fromName = '',
-    nb_breaking_subject: subject = '',
-    nb_breaking_preview: preview = '',
-    nb_breaking_list: list = [],
-    nb_breaking_header_img: image = 0, // eslint-disable-line
-    nb_breaking_should_send: send = false,
-    nb_breaking_content: content = '', // eslint-disable-line
-    nb_newsletter_sent_breaking_post_id: sentBreakingPostId = [],
-  } = meta;
 
   const {
     postId,
@@ -90,12 +77,12 @@ function EmailSettings() {
       .map((item: ListResult) => ({ label: item.Name, value: item.ListID }));
   }, [lists]);
   const selectedEmailList = useMemo(() => emailListOptions
-    .filter((item: Option) => list.includes(item.value)), [list, emailListOptions]);
+    .filter((item: Option) => meta.list.includes(item.value)), [meta.list, emailListOptions]);
 
-  const manualSubject = subject !== '';
-  const manualPreview = preview !== '';
+  const manualSubject = meta.subject !== '';
+  const manualPreview = meta.preview !== '';
 
-  const listArray = Array.isArray(list) ? list : [list];
+  const listArray = Array.isArray(meta.list) ? meta.list : [meta.list];
 
   const {
     newsletterBuilder: {
@@ -125,12 +112,15 @@ function EmailSettings() {
     setMeta({ nb_breaking_content: serialize(blocks) });
   };
 
-  const disabled = type === ''
-    || template === ''
-    || fromName === ''
-    || (subject === '' && postTitle === '')
-    || (preview === '' && postExcerpt === '')
-    || list.length === 0;
+  const disabled = meta.type === ''
+    || meta.template === ''
+    || meta.fromName === ''
+    || (meta.subject === '' && postTitle === '')
+    || (meta.preview === '' && postExcerpt === '')
+    || meta.list.length === 0;
+
+  console.log('rendering', emailListOptions);
+
   return (
     <PluginSidebar
       icon="email-alt2"
@@ -145,22 +135,22 @@ function EmailSettings() {
           contentHandler={contentHandler}
           typeHandler={(newType) => { setMeta({ nb_breaking_email_type: newType }); }}
           imageHandler={(newImage) => { setMeta({ nb_breaking_header_img: newImage }); }}
-          typeValue={type}
+          typeValue={meta.type}
           templateHandler={(newTemplate) => { setMeta({ nb_breaking_template: newTemplate }); }}
           fromNameHandler={(newFromName) => { setMeta({ nb_breaking_from_name: newFromName }); }}
-          templateValue={template}
-          fromNameValue={fromName}
+          templateValue={meta.template}
+          fromNameValue={meta.fromName}
         />
         <TextareaControl
           label={manualSubject ? __('Subject', 'wp-newsletter-builder') : __('Subject (linked)', 'wp-newsletter-builder')}
           placeholder={__('Enter subject', 'wp-newsletter-builder')}
-          value={subject !== '' ? subject : postTitle}
+          value={meta.subject !== '' ? meta.subject : postTitle}
           onChange={(value) => { setMeta({ nb_breaking_subject: value }); }}
         />
         <TextareaControl
           label={manualPreview ? __('Preview Text', 'wp-newsletter-builder') : __('Preview Text (linked)', 'wp-newsletter-builder')}
           placeholder={__('Enter preview text', 'wp-newsletter-builder')}
-          value={preview !== '' ? preview : postExcerpt}
+          value={meta.preview !== '' ? meta.preview : postExcerpt}
           onChange={(value) => { setMeta({ nb_breaking_preview: value }); }}
         />
         {Object.keys(breakingLists).map((key: string) => {
@@ -200,36 +190,36 @@ function EmailSettings() {
         <div style={{ marginTop: '1rem' }}>
           <CheckboxControl
             label={postStatus === 'draft' || postStatus === 'auto-draft' ? __('Send Newsletter on Publish', 'wp-newsletter-builder') : __('Send Newsletter on Update', 'wp-newsletter-builder')}
-            checked={send && !disabled}
+            checked={meta.send && !disabled}
             onChange={(value) => { setMeta({ nb_breaking_should_send: value }); }}
             disabled={disabled}
           />
-          {!type ? (
+          {!meta.type ? (
             <p style={{ color: 'red' }}>{__('Header Type is Required', 'wp-newsletter-builder')}</p>
           ) : null}
-          {!template ? (
+          {!meta.template ? (
             <p style={{ color: 'red' }}>{__('Template is Required', 'wp-newsletter-builder')}</p>
           ) : null}
-          {!fromName ? (
+          {!meta.fromName ? (
             <p style={{ color: 'red' }}>{__('From Name is Required', 'wp-newsletter-builder')}</p>
           ) : null}
-          {!subject && !postTitle ? (
+          {!meta.subject && !postTitle ? (
             <p style={{ color: 'red' }}>{__('Subject is Required', 'wp-newsletter-builder')}</p>
           ) : null}
-          {!preview && !postExcerpt ? (
+          {!meta.preview && !postExcerpt ? (
             <p style={{ color: 'red' }}>{__('Preview Text is Required', 'wp-newsletter-builder')}</p>
           ) : null}
-          {list.length === 0 ? (
+          {meta.list.length === 0 ? (
             <p style={{ color: 'red' }}>{__('Email List is Required', 'wp-newsletter-builder')}</p>
           ) : null}
         </div>
       </PanelBody>
-      {sentBreakingPostId ? (
+      {meta.sentBreakingPostId ? (
         <PanelBody
           initialOpen={false}
           title={__('Sent Newsletters', 'wp-newsletter-builder')}
         >
-          {sentBreakingPostId.map((id: number) => (
+          {meta.sentBreakingPostId.map((id: number) => (
             <SentNewsletter postId={id} key={id} />
           ))}
         </PanelBody>
---
+++ plugins/newsletter-from-post/useNewsletterMeta.tsx
@@ -0,0 +1,36 @@
+import { usePostMeta } from '@alleyinteractive/block-editor-tools';
+
+function useNewsletterMeta() {
+  const [meta, setMeta] = usePostMeta();
+
+  const {
+    nb_breaking_email_type: type = '',
+    nb_breaking_template: template = '',
+    nb_breaking_from_name: fromName = '',
+    nb_breaking_subject: subject = '',
+    nb_breaking_preview: preview = '',
+    nb_breaking_list: list = [],
+    nb_breaking_header_img: image = 0, // eslint-disable-line
+    nb_breaking_should_send: send = false,
+    nb_breaking_content: content = '', // eslint-disable-line
+    nb_newsletter_sent_breaking_post_id: sentBreakingPostId = [],
+  } = meta;
+
+  return {
+    meta: {
+      type,
+      template,
+      fromName,
+      subject,
+      preview,
+      list,
+      image,
+      send,
+      content,
+      sentBreakingPostId,
+    },
+    setMeta,
+  };
+}
+
+export default useNewsletterMeta;

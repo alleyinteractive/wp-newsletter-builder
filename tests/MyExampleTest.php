f76c050e0a9d66eaee45d6748540b69b84b6fc26
efuller <eric@alley.co>
2024-06-18 15:53:32 -0400

    Change example test name

--- tests/MyExampleTest.php
+++ tests/MyExampleTest.php
@@ -4,7 +4,7 @@ namespace WP_Newsletter_Builder;
 
 use WP_Newsletter_Builder\Tests\Test_Case;
 
-class Test_Example extends Test_Case {
+class MyExampleTest extends Test_Case {
 	public function test_example() {
 		$this->assertTrue( true );
 	}

7301bc651705a277d25d2ca12b8835ccbd4de3f0
cahdeemer <cahdeemer@users.noreply.github.com>
2024-06-18 13:17:01 +0000

    Built changes for v0.3.24 from develop 568cc0336bb920250943aa539b6e5428a366ff0e

---
+++ build/button/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '3d19c83ed6c4dc7b84e3');
---
+++ build/two-up-post/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => 'a6375aeff75acece83d5');

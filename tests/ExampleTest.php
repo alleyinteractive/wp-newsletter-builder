6ae9b90e6fde621b4274a8ac1b2ac30a3e2e9326
efuller <eric@alley.co>
2024-06-20 12:04:37 -0400

    Get tests working on PHPUnit 10

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,7 +4,7 @@ namespace WP_Newsletter_Builder;
 
 use WP_Newsletter_Builder\Tests\Test_Case;
 
-class Example_Test extends Test_Case {
+class ExampleTest extends Test_Case {
 	public function test_example() {
 		$this->assertTrue( true );
 	}

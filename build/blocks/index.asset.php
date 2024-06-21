f9ae79872f2519d7d36e2a88a06f94b8503a204e
cahdeemer <cahdeemer@users.noreply.github.com>
2024-06-21 17:22:36 +0000

    Built changes for v0.3.26 from develop 0f55ff0bf19a40a3ba9ed7b5005ab521f061a1f7

---
+++ build/blocks/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd66c72e6cb045a4fd15b');
---
+++ build/editor/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7a6c92a1a180cfde21b8');

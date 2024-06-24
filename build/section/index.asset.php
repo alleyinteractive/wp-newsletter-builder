2e8ac57273239b010cbcac329d75fce1aefeb44e
efuller <efuller@users.noreply.github.com>
2024-06-24 18:49:09 +0000

    Built changes for v0.3.28 from develop bc645b2ee1ac3a1ecb25cf01b3bc2d92291b7405

---
+++ build/section/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'df7cbd4eb11801db9c04');
